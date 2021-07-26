7bc5615c1d0bfd7ac3fe23fe3fdbb968402f8625
ojedam13 <m_ojeda17@hotmail.com>
2021-07-26 16:31:53 -0300

    css gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass') (require('sass'));
+var autoprefixer = require('gulp-autoprefixer');
 
 function css() {
     return gulp
         .src('scss/app.scss')
         .pipe(autoprefixer() )
         .pipe(sass({ outputStyle: "expanded" }))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest('css'))
 }
+
 function watchFiles() {
     gulp.watch('scss/*.scss', css);
     gulp.watch('index.html')
